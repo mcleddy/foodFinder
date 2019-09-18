be11081f40908cf8b2540bed5e098f5be14ac24c
Diana Gerardo <dianagerardo@email.arizona.edu>
2019-09-17 19:26:06 -0700

    Adding pseudocode to js

---
+++ code/app.js
@@ -0,0 +1,10 @@
+// Create click event for search your recipe button
+// This should show the ingredient form with the list of ingredients
+// Create click events for for each individual ingredients
+    // when an ingredient is clicked from the list, add them to the list of ingredients, add all the ingredients from each form
+// When next button is clicked, move on to next ingredient slide
+// When previous butto is clicked, move to previous slide of ingredients
+// When submit button is clicked, run ajax call that gathers up all the ingredients in the list of ingredients
+// response should show up in the results page, with the recipe cards
+// create click event for the recipe
+    // when recipe is clicked show all the recipe's information
