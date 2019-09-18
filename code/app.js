11e4f2bbfc8afb2640ec7faa2d087668adbdc251
Dylan Baker <Dylanbaker321@gmail.com>
2019-09-17 18:35:57 -0700

    Added api Key Baisc

---
+++ code/app.js
@@ -0,0 +1,7 @@
+var queryURL = "https://api.edamam.com/search?q=" + searchTerm + "&app_id=$"+ appId +"&app_key=$" +api_key+ "&from=0&to=5";
+
+var api_key = "bd3f05a4afcbd7d5d08c20e7058187df";
+
+var appId = "1388115a";
+
+var searchTerm = "chicken"
